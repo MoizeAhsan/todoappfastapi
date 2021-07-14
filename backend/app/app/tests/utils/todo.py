b769e1743eedff039fe4d468eb66e65767c1964b
Moize Ahsan <moizeahsan@baarei.com>
2021-07-14 22:06:38 +0500

    test: (utils) todo.py
    - added utility function to create random todo

---
+++ backend/app/app/tests/utils/todo.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from app import crud, models
+from app.schemas.todo import TodoCreate
+from app.tests.utils.user import create_random_user
+from app.tests.utils.utils import random_lower_string
+
+
+def create_random_todo(db: Session, *, owner_id: Optional[int] = None) -> models.Todo:
+    if owner_id is None:
+        user = create_random_user(db)
+        owner_id = user.id
+    title = random_lower_string()
+    description = random_lower_string()
+    item_in = TodoCreate(title=title, description=description, id=id)
+    return crud.todo.create_with_owner(db=db, obj_in=item_in, owner_id=owner_id)
